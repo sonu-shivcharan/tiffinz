7e6fbac94e35f8442aed3ba91735cfa74add7d2f
sonu-shivcharan <sonushivcharan@outlook.com>
2026-05-03 13:53:03 +0530

    feat : add rate limiter to register user route

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -5,31 +5,39 @@ import { registerUser } from "@/helpers/server/user.auth";
 import { UserRole } from "@/constants/enum";
 import { IUser } from "@/models/user.model";
 
-export const POST = asyncHandler(async (req) => {
-  const body = await req.json();
-  // console.log('body', body)
-  const parseResult = userSchema.safeParse(body);
-  if (!parseResult.success) {
-    return ApiResponse.zodError(parseResult.error);
-  }
+export const POST = asyncHandler(
+  async (req) => {
+    const body = await req.json();
+    // console.log('body', body)
+    const parseResult = userSchema.safeParse(body);
+    if (!parseResult.success) {
+      return ApiResponse.zodError(parseResult.error);
+    }
 
-  const userData = parseResult.data;
+    const userData = parseResult.data;
 
-  if (
-    userData.role === UserRole.admin &&
-    userData.adminSecret !== process.env.ADMIN_SECRET
-  ) {
-    console.info(
-      `Invalid Secret, registering user with default privileges. FullName: ${userData.fullName}`
+    if (
+      userData.role === UserRole.admin &&
+      userData.adminSecret !== process.env.ADMIN_SECRET
+    ) {
+      console.info(
+        `Invalid Secret, registering user with default privileges. FullName: ${userData.fullName}`,
+      );
+      userData.role = UserRole.user;
+    }
+    console.log("userData", userData);
+    const createdUser = await registerUser(userData as IUser);
+    const userType = userData.role === UserRole.admin ? "Admin" : "User";
+    return ApiResponse.success(
+      `${userType} registered successfully`,
+      { user: createdUser },
+      201,
     );
-    userData.role = UserRole.user;
-  }
-console.log('userData', userData)
-  const createdUser = await registerUser(userData as IUser);
-  const userType = userData.role === UserRole.admin ? "Admin" : "User";
-  return ApiResponse.success(
-    `${userType} registered successfully`,
-    { user: createdUser },
-    201
-  );
-});
+  },
+  {
+    rateLimiter: {
+      maxReq: 5,
+      timeout: 60,
+    },
+  },
+);
