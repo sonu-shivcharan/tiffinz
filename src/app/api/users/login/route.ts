d965fb49cb19f7f2cb0e25473195c6fc671c58fe
sonu-shivcharan <sonushivcharan@outlook.com>
2026-05-03 13:51:21 +0530

    fix: enhance asyncHandler with rate limiting functionality

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -22,49 +22,57 @@ import { loginUser, createUserSession } from "@/helpers/server/user.auth";
 //   { key: "phone", schema: loginWithPhoneSchema },
 // ];
 
-export const POST = asyncHandler(async (req) => {
-  const body: ILoginCredentials = await req.json();
-  type LoginKey = "email" | "username" | "phone";
+export const POST = asyncHandler(
+  async (req) => {
+    const body: ILoginCredentials = await req.json();
+    type LoginKey = "email" | "username" | "phone";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const loginOptions: { key: LoginKey; schema: any }[] = [
-  { key: "email", schema: loginWithEmailSchema },
-  { key: "username", schema: loginWithUsernameSchema },
-  { key: "phone", schema: loginWithPhoneSchema },
-];
-  let validatedData: {
-  key: LoginKey;
-  value: string;
-  password: string;
-} | null = null;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const loginOptions: { key: LoginKey; schema: any }[] = [
+      { key: "email", schema: loginWithEmailSchema },
+      { key: "username", schema: loginWithUsernameSchema },
+      { key: "phone", schema: loginWithPhoneSchema },
+    ];
+    let validatedData: {
+      key: LoginKey;
+      value: string;
+      password: string;
+    } | null = null;
 
-  for (const option of loginOptions) {
-    if (option.key in body) {
-      const parsed = option.schema.safeParse(body);
-      if (!parsed.success) {
-        return ApiResponse.zodError(parsed.error);
+    for (const option of loginOptions) {
+      if (option.key in body) {
+        const parsed = option.schema.safeParse(body);
+        if (!parsed.success) {
+          return ApiResponse.zodError(parsed.error);
+        }
+        const { password } = parsed.data;
+        validatedData = {
+          key: option.key,
+          value: parsed.data[option.key],
+          password,
+        };
+        break;
       }
-      const { password } = parsed.data;
-      validatedData = {
-        key: option.key,
-        value: parsed.data[option.key],
-        password,
-      };
-      break;
     }
-  }
-  if (!validatedData) {
-    return ApiResponse.error("No valid login identifier provided", 400);
-  }
-  console.log('validatedData', validatedData)
+    if (!validatedData) {
+      return ApiResponse.error("No valid login identifier provided", 400);
+    }
+    console.log("validatedData", validatedData);
 
-  const loginResult = await loginUser(
-    validatedData.key,
-    validatedData.value,
-    validatedData.password  
-  );
-  
-  const { user } = loginResult;
+    const loginResult = await loginUser(
+      validatedData.key,
+      validatedData.value,
+      validatedData.password,
+    );
 
-  return await createUserSession(user?._id);
-});
+    const { user } = loginResult;
+
+    return await createUserSession(user?._id);
+  },
+  {
+    rateLimiter: {
+      maxReq: 4,
+      timeout: 60,
+    },
+  },
+);
--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,13 +1,42 @@
 import { handleError } from "@/utils/handleError";
 import { NextRequest } from "next/server";
+import { redis } from "./redis";
+import { ApiError } from "./apiError";
+
 type RouteContext<T = Record<string, never>> = {
   params: Promise<T>;
 };
+
+type AsyncHandlerOptions = {
+  rateLimiter?: {
+    maxReq: number;
+    timeout: number;
+  };
+};
 export function asyncHandler<T = Record<string, never>>(
   handler: (req: NextRequest, context: RouteContext<T>) => Promise<Response>,
+  options?: AsyncHandlerOptions,
 ) {
   return async function (req: NextRequest, context: RouteContext<T>) {
     try {
+      if (options?.rateLimiter) {
+        const ip =
+          req.headers.get("x-forwarded-for")?.split(",")[0] ||
+          req.headers.get("x-real-ip") ||
+          req.headers.get("x-vercel-ip") ||
+          "unknown";
+        // console.log("options", options, "IP:", ip);
+        const rateLimiterKey = `rate_limit:${ip}`;
+        const limit = options.rateLimiter.maxReq || 5;
+        const window = options.rateLimiter.timeout;
+        const current = await redis.incr(rateLimiterKey);
+        if (current === 1) {
+          redis.expire(rateLimiterKey, window);
+        }
+        if (current > limit) {
+          throw new ApiError("Too many requests, try later", 429);
+        }
+      }
       return await handler(req, context);
     } catch (error) {
       return handleError(error);
