5167118bd1e12e6f4ad5f873de007339e212719d
sonu-shivcharan <sonushivcharan@outlook.com>
2026-05-04 11:34:14 +0530

    fix: format account model for improved readability

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,31 +1,31 @@
 import mongoose, { model, models, Schema } from "mongoose";
 import { IUser } from "./user.model";
-interface IAccount extends mongoose.Document{
-    _id:mongoose.Types.ObjectId;
-    user:mongoose.Types.ObjectId;
-    balance:number;
-    createdAt?:Date;
-    updatedAt?:Date;
+interface IAccount extends mongoose.Document {
+  _id: mongoose.Types.ObjectId;
+  user: mongoose.Types.ObjectId;
+  balance: number;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-const accountSchema = new Schema<IAccount>({
-    balance:{
-        type:Number,
-        default:0,
+const accountSchema = new Schema<IAccount>(
+  {
+    balance: {
+      type: Number,
+      default: 0,
     },
-    user:{
-        type:Schema.Types.ObjectId,
-        ref:"User",
-        required:true
-    }
-},
-    { timestamps: true }
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
 );
 
-
-const Account= models?.Account || model<IAccount>("Account", accountSchema);
+const Account = models?.Account || model<IAccount>("Account", accountSchema);
 type IAccountWithUser = Omit<IAccount, "user"> & {
-  user: IUser
-}
+  user: IUser;
+};
 export type { IAccount, IAccountWithUser };
 export default Account;
