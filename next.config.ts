3e61bdaa375c0e1cd34778a5d20acbec2397e568
sonu-shivcharan <sonushivcharan@outlook.com>
2026-06-03 19:57:05 +0530

    allow new dev origin

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("https://ik.imagekit.io/**"), new URL("https://example.com/**")],
+    remotePatterns: [
+      new URL("https://ik.imagekit.io/**"),
+      new URL("https://example.com/**"),
+    ],
   },
+
+  allowedDevOrigins: ["192.168.1.2"],
 };
 
 export default nextConfig;
