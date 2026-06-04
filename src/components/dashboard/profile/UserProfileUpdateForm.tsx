3917c1493aa3a4cecbd6244532711c52c9430c64
sonu-shivcharan <sonushivcharan@outlook.com>
2026-06-05 00:04:18 +0530

    feat: add image crop preview with draging and scaling

--- src/components/dashboard/profile/UserProfileUpdateForm.tsx
+++ src/components/dashboard/profile/UserProfileUpdateForm.tsx
@@ -11,8 +11,7 @@ import { Input } from "@/components/ui/input";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { Camera } from "lucide-react";
-import Image from "next/image";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function UserProfileUpdateForm() {
   const { user } = useCurrentUser();
@@ -33,9 +32,12 @@ const AvatarUpload = () => {
   const avatarUploadRef = useRef<HTMLInputElement>(null);
   const [avatarEditorOpen, setAvatarEditorOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  useEffect(() => {
+    // return () => {
+    //   setSelectedImage(null);
+    // };
+  }, [selectedImage]);
   const handleAvatarClick = () => {
-    setSelectedImage(null);
-    setAvatarEditorOpen(false);
     avatarUploadRef.current?.click();
   };
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -46,7 +48,7 @@ const AvatarUpload = () => {
       setAvatarEditorOpen(true);
     }
   };
-  if (!user) return null;
+
   return (
     <div>
       <Input
@@ -92,26 +94,97 @@ const SelectedImagePreview = ({
 }: {
   selectedImage?: File | null;
 }) => {
+  const [scale, setScale] = useState(50);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
+  const [dragOrigin, setDragOrigin] = useState({ x: 0, y: 0 });
+  const imageRef = useRef<HTMLImageElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const handlePoiterDown = (e: React.PointerEvent<HTMLImageElement>) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    setDragStart({ x, y });
+    setDragOrigin(position);
+    console.log("poiner down", { x, y });
+  };
+  const handlePointerMove = (e: React.PointerEvent<HTMLImageElement>) => {
+    if (e.buttons !== 1) return; // Only move when left mouse button is pressed
+
+    e.currentTarget.setPointerCapture(e.pointerId);
+    const x = e.clientX;
+    const y = e.clientY;
+    const deltaX = x - dragStart.x;
+    const deltaY = y - dragStart.y;
+    setPosition({
+      x: dragOrigin.x + deltaX,
+      y: dragOrigin.y + deltaY,
+    });
+    console.log("poiner move", { x, y }, { deltaX, deltaY });
+  };
+  const handlePointerUp = (e: React.PointerEvent<HTMLImageElement>) => {
+    if (e.buttons !== 1) return; // Only move when left mouse button is released
+    e.currentTarget.releasePointerCapture(e.pointerId);
+    const x = e.clientX;
+    const y = e.clientY;
+    const deltaX = x - dragStart.x;
+    const deltaY = y - dragStart.y;
+    setPosition((prev) => ({
+      x: prev.x + deltaX,
+      y: prev.y + deltaY,
+    }));
+  };
+  const handleScaleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newScale = parseFloat(e.target.value);
+    setScale(newScale);
+  };
+  const handleImageSave = () => {
+    const canvas = canvasRef.current;
+    if (canvas && imageRef.current) {
+      const ctx = canvas.getContext("2d");
+      ctx?.clearRect(0, 0, canvas.width, canvas.height);
+      ctx?.drawImage(imageRef.current, 0, 0);
+    }
+  };
+  // console.log(position);
+
   return (
     <div className="flex-col items-center justify-center">
       <div
         id="image-crop-area"
-        className="w-[400px] h-[400px] bg-muted overflow-visible border-4 border-blue-500 mx-auto"
+        className="w-full aspect-square md:w-[400px] md:h-[400px] bg-muted overflow-hidden border-4 border-blue-500 mx-auto"
       >
         {selectedImage && (
-          <Image
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            ref={imageRef}
             src={URL.createObjectURL(selectedImage!)}
             alt="Selected Avatar"
             width={200}
             height={200}
-            className="h-full w-full object-cover cursor-move"
-            onMouseMove={(e) => {
-              console.log("Mouse move", e.clientX, e.clientY);
+            onPointerDown={handlePoiterDown}
+            onPointerMove={handlePointerMove}
+            onPointerUp={handlePointerUp}
+            draggable={false}
+            className="w-full h-fit cursor-move select-none duration-10"
+            style={{
+              transform: `translate(${position.x}px, ${position.y}px) scale(${scale * 0.05})`,
             }}
           />
         )}
       </div>
-      <Input type="range" min="0" max="100" className="w-full mt-4" />
+      <Input
+        type="range"
+        min="1"
+        max="100"
+        value={scale}
+        onChange={handleScaleChange}
+        className="w-full mt-4 transition-all duration-300"
+      />
+      <Button onClick={handleImageSave} variant={"outline"}>
+        Save
+      </Button>
+      <canvas ref={canvasRef} className=""></canvas>
     </div>
   );
 };
