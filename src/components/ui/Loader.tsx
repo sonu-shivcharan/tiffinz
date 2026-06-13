b1f9d0fa85fa14b9f28b185ad070970397421ef1
sonu-shivcharan <sonushivcharan@outlook.com>
2026-06-13 14:29:18 +0530

    update loader to add loading text

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,10 +1,14 @@
 import { Loader2 } from "lucide-react";
 import React from "react";
 
-function Loader() {
+interface LoaderProps {
+  text?: string;
+}
+function Loader({ text }: LoaderProps) {
   return (
-    <div className=" w-full h-full flex justify-center items-center mx-auto">
-      <Loader2 className="h-12 w-12 animate-spin duration-100"></Loader2>
+    <div className=" w-full h-full flex justify-center items-center mx-auto gap-2">
+      <Loader2 className="h-12 w-12 animate-spin duration-100"></Loader2>{" "}
+      {text && <span>{text}</span>}
     </div>
   );
 }
