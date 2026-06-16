6c326b66a8d4e584d62e85422f1a14075fec8c45
sonu-shivcharan <sonushivcharan@outlook.com>
2026-06-16 22:20:17 +0530

    enable rate limiter for password reset and verify reset token route

--- src/app/api/users/reset-password/route.ts
+++ src/app/api/users/reset-password/route.ts
@@ -6,25 +6,33 @@ import { ApiError } from "@/utils/apiError";
 import { ApiResponse } from "@/utils/ApiResponse";
 import { asyncHandler } from "@/utils/asyncHandler";
 
-const resetPasswordRoute = asyncHandler(async (req) => {
-  const { token, newPassword, userId } = await req.json();
-  if (!token) {
-    throw new ApiError("Token is required", 400);
-  }
-  if (!newPassword || newPassword.trim() === "") {
-    throw new ApiError("New password is required", 400);
-  }
+const resetPasswordRoute = asyncHandler(
+  async (req) => {
+    const { token, newPassword, userId } = await req.json();
+    if (!token) {
+      throw new ApiError("Token is required", 400);
+    }
+    if (!newPassword || newPassword.trim() === "") {
+      throw new ApiError("New password is required", 400);
+    }
 
-  const isTokenValid = await verifyPasswordResetToken(token, userId);
-  if (!isTokenValid) {
-    throw new ApiError("Password reset token expired", 400);
-  }
-  const { success } = await resetPassword(userId, newPassword);
-  if (!success) {
-    throw new ApiError("Failed to reset password try again later", 500);
-  }
+    const isTokenValid = await verifyPasswordResetToken(token, userId);
+    if (!isTokenValid) {
+      throw new ApiError("Password reset token expired", 400);
+    }
+    const { success } = await resetPassword(userId, newPassword);
+    if (!success) {
+      throw new ApiError("Failed to reset password try again later", 500);
+    }
 
-  return ApiResponse.success("Password reset successfully");
-});
+    return ApiResponse.success("Password reset successfully");
+  },
+  {
+    rateLimiter: {
+      maxReq: 5,
+      timeout: 60,
+    },
+  },
+);
 
 export const POST = resetPasswordRoute;
--- src/app/api/users/reset-password/verify/route.ts
+++ src/app/api/users/reset-password/verify/route.ts
@@ -3,20 +3,28 @@ import { ApiError } from "@/utils/apiError";
 import { ApiResponse } from "@/utils/ApiResponse";
 import { asyncHandler } from "@/utils/asyncHandler";
 
-const verifyPasswordResetTokenRoute = asyncHandler(async (req) => {
-  const searchParams = req.nextUrl.searchParams;
-  const token = searchParams.get("token");
-  const userId = searchParams.get("id");
-  if (!token || !userId) {
-    throw new ApiError("Invalid password reset link");
-  }
+const verifyPasswordResetTokenRoute = asyncHandler(
+  async (req) => {
+    const searchParams = req.nextUrl.searchParams;
+    const token = searchParams.get("token");
+    const userId = searchParams.get("id");
+    if (!token || !userId) {
+      throw new ApiError("Invalid password reset link");
+    }
 
-  const isTokenValid = await verifyPasswordResetToken(token, userId);
-  if (!isTokenValid) {
-    throw new ApiError("Invalid or expired password reset token", 400);
-  }
+    const isTokenValid = await verifyPasswordResetToken(token, userId);
+    if (!isTokenValid) {
+      throw new ApiError("Invalid or expired password reset token", 400);
+    }
 
-  return ApiResponse.success("Password reset token is valid", { userId });
-});
+    return ApiResponse.success("Password reset token is valid", { userId });
+  },
+  {
+    rateLimiter: {
+      maxReq: 10,
+      timeout: 60,
+    },
+  },
+);
 
 export const GET = verifyPasswordResetTokenRoute;
