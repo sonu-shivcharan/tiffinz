675335c40ca635d124d8a0a1ae4ccec94a8b64b4
sonu-shivcharan <sonushivcharan@outlook.com>
2026-06-21 18:38:10 +0530

    feat: add custom loader animation to Loader component

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -7,7 +7,7 @@ interface LoaderProps {
 function Loader({ text }: LoaderProps) {
   return (
     <div className=" w-full h-full flex justify-center items-center mx-auto gap-2">
-      <Loader2 className="h-12 w-12 animate-spin duration-100"></Loader2>{" "}
+      <Loader2 className="h-12 w-12 animate-loader"></Loader2>{" "}
       {text && <span>{text}</span>}
     </div>
   );
